cbec5b81e7687f7a8fcb4252422fd007730ff6d8
randy0725@gmail.com <randy0725@gmail.com>
2016-03-15 00:03:29 +0800

    STFSPackage 修改完毕

--- CLKsFATXLib/STFS/STFSPackage.cs
+++ CLKsFATXLib/STFS/STFSPackage.cs
@@ -7,6 +7,10 @@ namespace CLKsFATXLib.STFS
 {
     class STFSPackage
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="aInStream"></param>
         public STFSPackage(System.IO.Stream aInStream)
         {
             PackageStream = aInStream;
@@ -70,28 +74,33 @@ namespace CLKsFATXLib.STFS
         }
 
         /// <summary>
-        /// Checks the file magic to check if it's a valid CON/LIVE/PIRS package
+        /// 计算文件魔数(Magic Number)，确认文件的类型
+        /// CON/LIVE/PIRS/NIGR 类型
         /// </summary>
         public bool IsSTFSPackage()
         {
+            // 1010 0000 0000 0000
             if (PackageStream.Length >= 0xA000)
             {
-                Streams.Reader r = new Streams.Reader(PackageStream);
-                r.BaseStream.Position = 0;
-                byte[] Buffer = r.ReadBytes(0x200);
+                uint sVal = 0;
+
+                Streams.Reader sReader = new Streams.Reader(PackageStream);
+                sReader.BaseStream.Position = 0;
+                // 512 Bytes
+                byte[] Buffer = sReader.ReadBytes(0x200);
                 
-                r = new Streams.Reader(new System.IO.MemoryStream(Buffer));
-                uint val = r.ReadUInt32();
-                
-                switch (val)
+                sReader = new Streams.Reader(new System.IO.MemoryStream(Buffer));
+                sVal = sReader.ReadUInt32();
+
+                switch (sVal)
                 {
-                    // CON
+                    // CON ‭ 1010 0011 0100 1111 0100 1110 0010 0000‬
                     case 0x434F4E20:
                         return true;
-                    // LIVE
+                    // LIVE ‭0100 1100 0100 1001 0101 0110 0100 0101‬
                     case 0x4C495645:
                         return true;
-                    // PIRS
+                    // PIRS ‭0101 0000 0100 1001 0101 0010 0101 0011‬
                     case 0x50495253:
                         return true;
                     // NIGR
@@ -106,66 +115,75 @@ namespace CLKsFATXLib.STFS
         }
 
         /// <summary>
-        /// If the file is an STFS package, it will get the package's display (content) name
+        /// 如果文件是STFS包，获取包的名称
         /// </summary>
-        /// <returns>Package display/content name</returns>
+        /// <returns>返回STFS包的名称</returns>
         public string ContentName()
         {
-            Streams.Reader r = new CLKsFATXLib.Streams.Reader(PackageStream);
-            r.BaseStream.Position = (long)Geometry.STFSOffsets.DisplayName;
-            return r.ReadUnicodeString(0x80);
+            Streams.Reader sReader = new Streams.Reader(PackageStream);
+            sReader.BaseStream.Position = (long)Geometry.STFSOffsets.DisplayName;
+
+            return sReader.ReadUnicodeString(0x80);
         }
 
         /// <summary>
-        /// If the file is an STFS package, it will get the name of the game that the package belongs to
+        /// 如果文件是STFS包，获取包中游戏名称
         /// </summary>
+        /// <returns>返回STFS包所属游戏名称</returns>
         public string TitleName()
         {
-            Streams.Reader r = new CLKsFATXLib.Streams.Reader(PackageStream);
-            r.BaseStream.Position = (long)Geometry.STFSOffsets.TitleName;
-            return r.ReadUnicodeString(0x80);
+            Streams.Reader sReader = new Streams.Reader(PackageStream);
+            sReader.BaseStream.Position = (long)Geometry.STFSOffsets.TitleName;
+
+            return sReader.ReadUnicodeString(0x80);
         }
 
         /// <summary>
-        /// If the file is an STFS package, it will get the game's unique identifier
+        /// 如果文件是STFS包，获取包中游戏识别符
         /// </summary>
+        /// <returns>返回STFS包中游戏识别符</returns>
         public uint TitleID()
         {
-            Streams.Reader r = new CLKsFATXLib.Streams.Reader(PackageStream);
-            r.BaseStream.Position = (long)Geometry.STFSOffsets.TitleID;
-            return r.ReadUInt32();
+            Streams.Reader sReader = new Streams.Reader(PackageStream);
+            sReader.BaseStream.Position = (long)Geometry.STFSOffsets.TitleID;
+
+            return sReader.ReadUInt32();
         }
 
         /// <summary>
-        /// If the file is an STFS package, it will get the package owner's profile ID
+        /// 如果文件是STFS包，获取包所有者配置文件ID
         /// </summary>
+        /// <returns>返回STFS包所有者配置文件ID</returns>
         public byte[] ProfileID()
         {
-            Streams.Reader r = new CLKsFATXLib.Streams.Reader(PackageStream);
-            r.BaseStream.Position = (long)Geometry.STFSOffsets.ProfileID;
-            return r.ReadBytes(0x8);
+            Streams.Reader sReader = new Streams.Reader(PackageStream);
+            sReader.BaseStream.Position = (long)Geometry.STFSOffsets.ProfileID;
+
+            return sReader.ReadBytes(0x8);
         }
 
         /// <summary>
-        /// If the file is an STFS package, it will get the device ID that the package was created on (HDD/USB)
+        /// 如果文件是STFS包，获取该包生成时设备ID(HDD/USB)
         /// </summary>
-        /// <returns>Package device ID</returns>
+        /// <returns>返回STFS包生成时设备ID</returns>
         public byte[] DeviceID()
         {
-            Streams.Reader r = new CLKsFATXLib.Streams.Reader(PackageStream);
-            r.BaseStream.Position = (long)Geometry.STFSOffsets.DeviceID;
-            return r.ReadBytes(0x14);
+            Streams.Reader sReader = new Streams.Reader(PackageStream);
+            sReader.BaseStream.Position = (long)Geometry.STFSOffsets.DeviceID;
+
+            return sReader.ReadBytes(0x14);
         }
 
         /// <summary>
-        /// If the file is an STFS package, it will get the identifier that the package was created on.
+        /// 如果文件是STFS包，获取该包生成时的识别符
         /// </summary>
-        /// <returns>Console ID for STFS package</returns>
+        /// <returns>返回STFS包生成时的识别符</returns>
         public byte[] ConsoleID()
         {
-            Streams.Reader r = new CLKsFATXLib.Streams.Reader(PackageStream);
-            r.BaseStream.Position = (long)Geometry.STFSOffsets.ConsoleID;
-            return r.ReadBytes(0x5);
+            Streams.Reader sReader = new Streams.Reader(PackageStream);
+            sReader.BaseStream.Position = (long)Geometry.STFSOffsets.ConsoleID;
+
+            return sReader.ReadBytes(0x5);
         }
 
         private System.IO.Stream PackageStream
